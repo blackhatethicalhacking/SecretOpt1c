49ef3104507c052603766ed1000eabd3d063f3e9
Black Hat Ethical Hacking <chris@blackhatethicalhacking.com>
2023-02-13 16:35:49 +0200

    Update S3cr3tDetect0rz.sh

--- S3cr3tDetect0rz.sh
+++ S3cr3tDetect0rz.sh
@@ -56,8 +56,7 @@ if [ ! -f "$domain/discovered_urls_for_$domain.txt" ]; then
   exit 1
 fi
 count=`grep -E $(cat secrethub.json | jq -r '.patterns | join("|")') "$domain/discovered_urls_for_$domain.txt" | awk 'BEGIN {count=0} {count++} END {print count}'`
-grep -E $(cat secrethub.json | jq -r '.patterns | join("|")') "$domain/discovered_urls_for_$domain.txt" | awk '{print $0}' > "$domain/secrets.csv"
-
+grep -E -o "$(cat secrethub.json | jq -r '.patterns | join("|")')" "$domain/discovered_urls_for_$domain.txt" | awk '{print $0 "," FILENAME}' > "$domain/secrets.csv"
 # Print summary of secrets found
 echo "Total secrets found: $count" | lolcat
 echo "Offense is the best Defense baby!" | lolcat

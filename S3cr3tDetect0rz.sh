eed87e975b23df3e3d79ecf18917c98db1ea99f3
Black Hat Ethical Hacking <chris@blackhatethicalhacking.com>
2023-02-13 17:22:41 +0200

    Update S3cr3tDetect0rz.sh

--- S3cr3tDetect0rz.sh
+++ S3cr3tDetect0rz.sh
@@ -47,17 +47,22 @@ grep "Status: 301" $domain/gobuster.txt | grep -oE "(http|https)://[a-zA-Z0-9./?
 # Loop through each URL and run curl
 echo "Performing curl on every URL I found to fetch the content..." | lolcat
 while read discovered_url; do
-  curl -s $discovered_url > $domain/discovered_urls_for_$(echo $discovered_url | awk -F/ '{print $3}').txt
+  content=$(curl -s $discovered_url)
+  if [ ! -z "$content" ]; then
+    echo $content > "$domain/discovered_urls_for_$(echo $discovered_url | awk -F/ '{print $3}').txt"
+  fi
 done < $domain/discovered_urls.txt
 # Search for secrets in the output of curl and save the result in secrets.csv
 echo "I am now searching for Secrets using secrethub.json and saving the results in secrets.csv for you..." | lolcat
-if [ ! -f "$domain/discovered_urls_for_$domain.txt" ]; then
-  echo "No discovered_urls_for_$domain file found." | lolcat
-  exit 1
-fi
-count=`grep -E $(cat secrethub.json | jq -r '.patterns | join("|")') "$domain/discovered_urls_for_$domain.txt" | awk 'BEGIN {count=0} {count++} END {print count}'`
-grep -E $(cat secrethub.json | jq -r '.patterns | join("|")') "$domain/discovered_urls_for_$domain.txt" | awk -v url="$discovered_url" '{print url "," $0}' > "$domain/secrets.csv"
-
+while read discovered_url; do
+  if [ -f "$domain/discovered_urls_for_$(echo $discovered_url | awk -F/ '{print $3}').txt" ]; then
+    count=$(grep -E $(cat secrethub.json | jq -r '.patterns | join("|")') "$domain/discovered_urls_for_$(echo $discovered_url | awk -F/ '{print $3}').txt" | awk 'BEGIN {count=0} {count++} END {print count}')
+    if [ "$count" -ne 0 ]; then
+      echo "$discovered_url" >> "$domain/secrets.csv"
+      grep -E $(cat secrethub.json | jq -r '.patterns | join("|")') "$domain/discovered_urls_for_$(echo $discovered_url | awk -F/ '{print $3}').txt" | awk '{print $0}' >> "$domain/secrets.csv"
+    fi
+  fi
+done < $domain/discovered_urls.txt
 # Print summary of secrets found
 echo "Total secrets found: $count" | lolcat
 echo "Offense is the best Defense baby!" | lolcat

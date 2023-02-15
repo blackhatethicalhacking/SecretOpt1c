b9970ddfc4a49fc26fe8ef3b8aea1dbb29ea19c1
Black Hat Ethical Hacking <chris@blackhatethicalhacking.com>
2023-02-16 00:43:20 +0200

    Update S3cr3tDetect0rz.sh

--- S3cr3tDetect0rz.sh
+++ S3cr3tDetect0rz.sh
@@ -1,29 +1,4 @@
 #!/bin/bash
-curl --silent "https://raw.githubusercontent.com/blackhatethicalhacking/Subdomain_Bruteforce_bheh/main/ascii.sh" | lolcat
-echo ""
-# Generate a random Sun Tzu quote for offensive security
-# Array of Sun Tzu quotes
-quotes=("The supreme art of war is to subdue the enemy without fighting." "All warfare is based on deception." "He who knows when he can fight and when he cannot, will be victorious." "The whole secret lies in confusing the enemy, so that he cannot fathom our real intent." "To win one hundred victories in one hundred battles is not the acme of skill. To subdue the enemy without fighting is the acme of skill.")
-# Get a random quote from the array
-random_quote=${quotes[$RANDOM % ${#quotes[@]}]}
-# Print the quote
-echo "Offensive Security Tip: $random_quote - Sun Tzu" | lolcat
-sleep 1
-echo "MEANS, IT'S ☕ 1337 ⚡ TIME, 369 ☯ " | lolcat
-sleep 1
-figlet -w 80 -f small S3cr3tDetect0rz | lolcat
-echo ""
-echo "[YOUR ARE USING S3cr3tDetect0rz.sh] - (v1.0) CODED BY Chris 'SaintDruG' Abou-Chabké WITH ❤ FOR blackhatethicalhacking.com for Educational Purposes only!" | lolcat
-sleep 1
-#check if the user is connected to the internet
-tput bold;echo "CHECKING IF YOU ARE CONNECTED TO THE INTERNET!" | lolcat
-# Check connection
-wget -q --spider https://google.com
-if [ $? -ne 0 ];then
-    echo "++++ CONNECT TO THE INTERNET BEFORE RUNNING S3cr3tDetect0rz.sh!" | lolcat
-    exit 1
-fi
-tput bold;echo "++++ CONNECTION FOUND, LET'S GO!" | lolcat
 # Take input for URL and path to wordlist
 read -p "Enter the URL: " url
 read -p "Enter path to wordlist: " wordlist
@@ -37,8 +12,6 @@ if [ ! -f "$wordlist" ]; then
 fi
 
 # Create a directory to store the results of curl using the domain name of the URL provided by the user
-mkdir -p "results/$(echo "$url" | cut -d/ -f3)"
-sleep 1
 domain="$(echo "$url" | cut -d/ -f3)"
 mkdir -p "$domain"
 
@@ -54,7 +27,7 @@ grep "Status: 301" "$domain/gobuster.txt" | grep -oE "(http|https)://[a-zA-Z0-9.
 # Use xargs and curl to fetch the content of each discovered URL in parallel
 echo "Performing curl on every URL I found to fetch the content..." | lolcat
 sleep 1
-cat "$domain/discovered_urls.txt" | xargs -I{} -P 10 sh -c 'curl -vsS -n {} > "$domain/discovered_urls_for_$(echo {} | awk -F/ '\''{print $3}'\'').txt" 2>&1'
+cat "$domain/discovered_urls.txt" | xargs -I{} -P 10 sh -c 'curl -sS -# -n {} > "$domain/discovered_urls_for_$(echo {} | awk -F/ '\''{print $3}'\'').txt" 2>&1'
 
 # Search for secrets in the output of curl and save the result in secrets.csv
 echo "Searching for secrets using secrethub.json and saving the results in secrets.csv for you..." | lolcat
@@ -80,3 +53,4 @@ done < "$domain/discovered_urls.txt"
 
 echo "Scan complete." | lolcat
 echo "You can view the results of the scan in the directory named $domain" | lolcat
+
